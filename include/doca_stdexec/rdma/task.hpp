e107536c9da8f2db3c64e63e5dda5f1cf3dd5ab5
Hongtao <hongtao_zhang@outlook.com>
2025-07-23 18:35:16 +0000

    use xmake for build and use latest stdexec (looks like error requires move which is not clear why)

--- include/doca_stdexec/rdma/task.hpp
+++ include/doca_stdexec/rdma/task.hpp
@@ -18,96 +18,93 @@ namespace doca_stdexec::rdma::task {
 
 template <typename T>
 concept DocaTask = requires(T t) {
-  { t.as_task() } -> std::same_as<doca_task *>;
+    { t.as_task() } -> std::same_as<doca_task*>;
 };
 
 template <typename Receiver, DocaTask DocaTask>
 struct rdma_operation : immovable {
+    using set_value_cb = void (*)(rdma_operation*);
+    using set_error_cb = void (*)(rdma_operation*, doca_error_t);
+    using set_stopped_cb = void (*)(rdma_operation*);
 
-  using set_value_cb = void (*)(rdma_operation *);
-  using set_error_cb = void (*)(rdma_operation *, doca_error_t);
-  using set_stopped_cb = void (*)(rdma_operation *);
+    set_value_cb set_value_callback = set_value;
+    set_error_cb set_error_callback = set_error;
+    set_stopped_cb set_stopped_callback = set_stopped;
 
-  set_value_cb set_value_callback = set_value;
-  set_error_cb set_error_callback = set_error;
-  set_stopped_cb set_stopped_callback = set_stopped;
+    rdma_operation(DocaTask task, Receiver receiver) : task(std::move(task)), receiver(std::move(receiver)) {}
 
-  rdma_operation(DocaTask task, Receiver receiver)
-      : task(std::move(task)), receiver(std::move(receiver)) {}
+    static void set_value(rdma_operation* op) {
+        op->receiver.set_value();
+    }
 
-  static void set_value(rdma_operation *op) { op->receiver.set_value(); }
+    static void set_error(rdma_operation* op, doca_error_t error) {
+        op->receiver.set_error(std::move(error));
+        check_error(error, "Operation Error");
+    }
 
-  static void set_error(rdma_operation *op, doca_error_t error) {
-    op->receiver.set_error(error);
-    check_error(error, "Operation Error");
-  }
+    static void set_stopped(rdma_operation* op) {
+        // TODO: Implement this
+    }
 
-  static void set_stopped(rdma_operation *op) {
-    // TODO: Implement this
-  }
-
-  void start() noexcept {
-    doca_task_set_user_data(task.as_task(), doca_data{.ptr = this});
-    auto status = doca_task_submit(task.as_task());
-    check_error(status, "Failed to submit task");
-  }
-  DocaTask task;
-  Receiver receiver;
+    void start() noexcept {
+        doca_task_set_user_data(task.as_task(), doca_data{.ptr = this});
+        auto status = doca_task_submit(task.as_task());
+        check_error(status, "Failed to submit task");
+    }
+    DocaTask task;
+    Receiver receiver;
 };
 
 template <DocaTask TaskType>
-inline void rdma_operation_set_value(typename TaskType::raw_type *raw_task,
-                                     doca_data user_data, doca_data ctx_data) {
-  auto task = TaskType{raw_task};
-  task.~TaskType(); // destroy the task
-  auto *op = static_cast<rdma_operation<uint8_t, TaskType> *>(user_data.ptr);
-  op->set_value_callback(op);
+inline void rdma_operation_set_value(typename TaskType::raw_type* raw_task, doca_data user_data, doca_data ctx_data) {
+    auto task = TaskType{raw_task};
+    task.~TaskType(); // destroy the task
+    auto* op = static_cast<rdma_operation<uint8_t, TaskType>*>(user_data.ptr);
+    op->set_value_callback(op);
 }
 
 template <DocaTask TaskType>
-inline void rdma_operation_set_error(typename TaskType::raw_type *raw_task,
-                                     doca_data user_data, doca_data ctx_data) {
-  auto *op = static_cast<rdma_operation<uint8_t, TaskType> *>(user_data.ptr);
-  auto task = TaskType{raw_task};
-  auto error = doca_task_get_status(task.as_task());
+inline void rdma_operation_set_error(typename TaskType::raw_type* raw_task, doca_data user_data, doca_data ctx_data) {
+    auto* op = static_cast<rdma_operation<uint8_t, TaskType>*>(user_data.ptr);
+    auto task = TaskType{raw_task};
+    auto error = doca_task_get_status(task.as_task());
 
-  printf("\n task: %p\n", &task);
+    printf("\n task: %p\n", &task);
 
-  op->set_error_callback(op, error);
+    op->set_error_callback(op, error);
 }
 
 template <DocaTask TaskType>
-inline void rdma_operation_set_stopped(typename TaskType::raw_type *raw_task,
-                                       doca_data user_data,
-                                       doca_data ctx_data) {
-  auto task = TaskType{raw_task};
-  auto *op = static_cast<rdma_operation<uint8_t, TaskType> *>(user_data.ptr);
-  op->set_stopped_callback(op);
+inline void rdma_operation_set_stopped(typename TaskType::raw_type* raw_task, doca_data user_data, doca_data ctx_data) {
+    auto task = TaskType{raw_task};
+    auto* op = static_cast<rdma_operation<uint8_t, TaskType>*>(user_data.ptr);
+    op->set_stopped_callback(op);
 }
 
-template <typename Task, typename... Buffers> struct rdma_sender {
+template <typename Task, typename... Buffers>
+struct rdma_sender {
+    using sender_concept = stdexec::sender_t;
 
-  using sender_concept = stdexec::sender_t;
+    using completion_signatures =
+        stdexec::completion_signatures<stdexec::set_value_t(), stdexec::set_error_t(doca_error_t)>;
 
-  using completion_signatures =
-      stdexec::completion_signatures<stdexec::set_value_t(),
-                                     stdexec::set_error_t(doca_error_t)>;
+    stdexec::env<> get_env() {
+        return {};
+    }
 
-  stdexec::env<> get_env() { return {}; }
+    template <stdexec::receiver Receiver>
+    auto connect(Receiver rcvr) {
+        return std::apply(
+            [&](auto&... buffers) {
+                auto task = Task::allocate(rdma, connection, buffers...);
+                return rdma_operation<Receiver, Task>{std::move(task), std::move(rcvr)};
+            },
+            _buffers);
+    }
 
-  template <stdexec::receiver Receiver> auto connect(Receiver rcvr) {
-    return std::apply(
-        [&](auto &...buffers) {
-          auto task = Task::allocate(rdma, connection, buffers...);
-          return rdma_operation<Receiver, Task>{std::move(task),
-                                                std::move(rcvr)};
-        },
-        buffers);
-  }
-
-  doca_rdma *rdma;
-  doca_rdma_connection *connection;
-  std::tuple<Buffers...> buffers;
+    doca_rdma* rdma;
+    doca_rdma_connection* connection;
+    std::tuple<Buffers...> _buffers;
 };
 
 } // namespace doca_stdexec::rdma::task
---
+++ xmake.lua
@@ -0,0 +1,40 @@
+set_project("doca-stdexec")
+set_version("0.1.0")
+set_languages("c++23")
+
+add_defines("DOCA_ALLOW_EXPERIMENTAL_API")
+set_policy("build.sanitizer.address", true)
+add_requires("pkgconfig::doca-argp", { alias = "doca-argp" })
+add_requires("pkgconfig::doca-aes-gcm", { alias = "doca-aes-gcm" })
+add_requires("pkgconfig::doca-comch", { alias = "doca-comch" })
+add_requires("pkgconfig::doca-common", { alias = "doca-common" })
+add_requires("pkgconfig::doca-dma", { alias = "doca-dma" })
+add_requires("pkgconfig::doca-rdma", { alias = "doca-rdma" })
+add_requires("pkgconfig::doca-sha", { alias = "doca-sha" })
+
+
+add_requires("stdexec main")
+
+
+target("doca-stdexec")
+    set_kind("headeronly")
+    add_includedirs("include", { public = true })
+
+add_packages(
+	"doca-argp",
+	"doca-aes-gcm",
+	"doca-comch",
+	"doca-common",
+	"doca-dma",
+	"doca-rdma",
+	"doca-sha",
+	{ public = true }
+)
+-- If there are headers to install, add them here e.g. add_headerfiles("include/(**.h)")
+
+target("rdma_loopback")
+    set_kind("binary")
+    add_files("test/rdma_loopback.cpp")
+    add_deps("doca-stdexec")
+    add_defines("DOCA_ALLOW_EXPERIMENTAL_API") -- Ensure define is applied to compilation
+    add_packages("stdexec")
