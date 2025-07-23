d063a91018ccbb7ead0a46e9f868e3eb284e9d63
Hongtao <hongtao_zhang@outlook.com>
2025-07-23 18:44:59 +0000

    test

--- xmake.lua
+++ xmake.lua
@@ -39,10 +39,10 @@ target("rdma_loopback")
     add_defines("DOCA_ALLOW_EXPERIMENTAL_API") -- Ensure define is applied to compilation
     add_packages("stdexec")
 
-package("foo")
-    set_description("The foo package")
+package("doca-stdexec")
+    set_description("The doca-stdexec package")
     set_license("Apache-2.0")
-    add_deps("bar") -- optional: add other package dependencies
+    add_deps("doca-stdexec") -- optional: add other package dependencies
 
     set_urls("https://github.com/taooceros/doca-stdexec.git")
     add_versions("0.1.0", "auto") -- specify version
