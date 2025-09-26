637ad497a528650c0b44ef60722e372b063b3698
Hongtao <hongtao_zhang@outlook.com>
2025-09-26 19:57:48 +0000

    add move constructor and assignment for pe_context

--- include/doca_stdexec/progress_engine.hpp
+++ include/doca_stdexec/progress_engine.hpp
@@ -278,6 +278,9 @@ public:
 
     doca_pe_context() : doca_pe_context(ProgressEngine{}) {}
 
+    doca_pe_context(doca_pe_context&&) = default;
+    doca_pe_context& operator=(doca_pe_context&&) = default;
+
     auto& get_pe() noexcept {
         return loop_.pe;
     }
